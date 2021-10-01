36e230ac73a46be4a6c782962a469dcddf3cc0c3
cmonsalt <cristiantm12@gmail.com>
2021-10-01 11:03:56 -0500

    Pintando prodcutos en el contenedor-productos

--- Desktop/CARRITO DE COMPRAS/app.js
+++ Desktop/CARRITO DE COMPRAS/app.js
@@ -1,13 +1,27 @@
-document.addEventListener(`DOMContentLoaded` , () =>{ 
-    fetchData()// Espera que se cargue todo el doc HTLM y ejecuta la funcion fetchData
-})
+document.addEventListener(`DOMContentLoaded`, () => {
+  fetchData(); // Espera que se cargue todo el doc HTLM y ejecuta la funcion fetchData
+});
 const fetchData = async () => {
   try {
-    const res = await fetch(`api.json`)
-    const data = await res.json( )
-    console.log(data)
+    const res = await fetch(`api.json`);
+    let data = await res.json();
+    pintarProductos(data);
+    // console.log(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
+//Pinta las cards en el contenedor-productos
+const contenedorProductos = document.querySelector(`#contenedor-productos`);
+const pintarProductos = (data) => {
+  const template = document.querySelector(`#template-productos`).content;
+  const fragment = document.createDocumentFragment();
+  data.forEach((producto) => {
+    template.querySelector(`img`).setAttribute(`src`, producto.image);
+
+    const clone = template.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  contenedorProductos.appendChild(fragment);
+};
