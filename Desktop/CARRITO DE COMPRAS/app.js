b416bc15113be350fd8924aae2855b465e699c91
cmonsalt <cristiantm12@gmail.com>
2021-10-05 15:52:49 -0500

    Ananse de proyecto

--- Desktop/CARRITO DE COMPRAS/app.js
+++ Desktop/CARRITO DE COMPRAS/app.js
@@ -30,6 +30,7 @@ const pintarProductos = (data) => {
   contenedorProductos.appendChild(fragment);
 };
 
+// Se crea objeto carrito y se realiza funcionalidad para que se agregen los productos al carrito
 let carrito = {};
 const detectarBotones = (data) => {
   const botones = document.querySelectorAll(`.card button`); //Detectar los Botones Comprar
@@ -38,25 +39,25 @@ const detectarBotones = (data) => {
       const producto = data.find(
         (item) => item.id === parseInt(btn.dataset.id) //Buscamos en la data el objeto que contiene el boton que se preciono y se agrega a la variable producto
       );
+
+      //Se agregan los productos al carrito si tienen stock suficiente
       producto.cantidad = 1;
       if (producto.stock === 0) {
         swal(
           "Oops!",
-          "No contamos con mas Unidades de este producto!",
+          "No contamos con mas unidades de este producto!",
           "error"
         );
       }
 
-      //Se agregan los productos al carrito si tienen stock suficiente
       if (producto.stock > 0) {
         if (carrito.hasOwnProperty(producto.id)) {
           producto.cantidad = carrito[producto.id].cantidad + 1;
         }
         carrito[producto.id] = { ...producto };
         producto.stock = carrito[producto.id].stock - 1;
+        pintarCarrito();
       }
-      //console.log(carrito);
-      pintarCarrito();
     });
   });
 };
@@ -77,4 +78,30 @@ const pintarCarrito = () => {
     fragment.appendChild(clone);
   });
   nombreProductos.appendChild(fragment);
+
+  pintarFooterCar();
+};
+
+//Pintar el footer del carrito
+const footerCar = document.querySelector(`#footer-car`);
+const pintarFooterCar = () => {
+  footerCar.innerHTML = ``;
+  const template = document.querySelector(`#template-footer`).content;
+  const fragment = document.createDocumentFragment();
+  const cantidadTotal = Object.values(carrito).reduce(
+    (acc, { cantidad }) => acc + cantidad,
+    0
+  ); // calcula la cantidad total de todos los producto presentes en el carrito
+
+  const precioTotal = Object.values(carrito).reduce(
+    (acc, { cantidad, unit_price }) => acc + cantidad * unit_price,
+    0
+  ); //Calcula el precio Total de todos los productos presentes en el carrito
+
+  template.querySelectorAll(`td`)[0].textContent = cantidadTotal;
+  template.querySelector(`span`).textContent = precioTotal;
+
+  const clone = template.cloneNode(true);
+  fragment.appendChild(clone);
+  footerCar.appendChild(fragment);
 };
